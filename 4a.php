e30d50458dd1b160c990cf4c1a2a41f940133255
Faizalhadziq <125478920+Faizalhadziq@users.noreply.github.com>
2023-03-13 15:53:30 +0700

    Add files via upload

---
+++ 4a.php
@@ -0,0 +1,7 @@
+<?php 
+
+$tulisan = ["epan","cipta","akan","u","asa","esok"];
+
+echo "M". $tulisan[4]. " d". $tulisan[0]. "mu di ". $tulisan[1]. "kan oleh apa yang kau kerj".$tulisan[2]. " hari ini, b". $tulisan[3]. "kan b". $tulisan[5];
+
+?>
---
+++ 4b.php
@@ -0,0 +1,29 @@
+<?php
+$hardware = ['Motherboard', 'Processor', 'Hard Disk', 'PC Coller', 'VGA Card', 'SSD'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h2>Macam-macam perangkat keras komputer</h2>
+    <ol>
+    <?php foreach ($hardware as $hw) : ?>
+        <li><?= $hw; ?></li>
+        <?php endforeach; ?>
+    </ol>
+    <h2>Macam-macam perangkat keras komputer baru</h2>
+    <ol>
+    <?php array_push($hardware, 'Card Reader', 'Modem') ?>
+    <?php sort($hardware); ?>
+    <?php foreach ($hardware as $hw) : ?>
+        <li><?= $hw; ?></li>
+        <?php endforeach; ?>
+    </ol>
+</body>
+</html>
